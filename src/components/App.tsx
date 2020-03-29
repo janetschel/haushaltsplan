dde2088a8d076864a44d2f193c1005f76f89c52a
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-29 13:08:27 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import Api from "../api/Api";
 
 function App() {
-
-  const test = async () => {
-    const response = await Api.getDocuments();
-    console.log(response);
-  };
-
-  test();
-
   return (
     <div className="App">
-      <p>Lask test to check if selfdeploy works</p>
+      <p>Testing</p>
     </div>
   );
 }
