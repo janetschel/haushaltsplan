9c64e62c08a13a6a40ba35bcdaaca6003edf9486
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-29 13:13:58 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 function App() {
+
+  const test = async () => {
+    const response = await fetch('https://haushaltsplan-backend.herokuapp.com/healthcheck');
+    console.log(response);
+  };
+
+  test();
+
   return (
     <div className="App">
       <p>Testing</p>
