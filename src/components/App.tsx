9af74e7eefe9595f04e6ab1d3ae795118d8326f7
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-30 11:32:45 +0200

    Reset everything for production env
    Reset every file so production is clean until next deploy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import Api from "../api/Api";
+import './App.css'
+import {Typography} from "@material-ui/core";
 
-function App() {
-  const performHealthCheck = async () =>  {
-    const response = await Api.getDocuments();
-    const json = await response.json();
-    console.log(json);
+class App extends React.Component<{}, { message: string }> {
+  constructor({props}: { props: any }) {
+    super(props);
+
+    this.state = {
+      message: '',
+    }
+  }
+
+  performHealthCheck = async () => {
+    const response = await (await Api.healthCheck()).text();
+    this.setState({ message: response });
   };
 
-  performHealthCheck();
+  render() {
+    const { message } = this.state;
+    this.performHealthCheck();
 
-  return (
-    <div className="App">
-      <p>Check console for healthcheck</p>
-    </div>
-  );
+    return(
+      <div className="App">
+        <Typography>Health check: {message}</Typography>
+        <Typography>Site under developement.. Please stay tuned</Typography>
+      </div>
+    );
+  }
 }
 
 export default App;
