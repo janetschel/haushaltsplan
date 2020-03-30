f336f4df84b0e6f6d0154f8ef67062363c6351a2
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-03-30 13:58:01 +0200

    Update Api.ts
    Added all REST functions for backend

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -9,15 +9,41 @@ const request = async (path: string, method: string) => {
   });
 };
 
+const requestWithBody = async (path: string, method: string, body: any) => {
+  const backendUrl = Config.getBackendUrl();
+  const fetchUrl = `${backendUrl}${path}`;
+
+  return await fetch(fetchUrl, {
+    method: method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+};
+
 const healthCheck = async () =>
     await request('/healthcheck', 'GET');
 
 const getDocuments = async () =>
   await request('/getDocuments', 'GET');
 
+const addDocument = async (document: {}) =>
+    await requestWithBody('/addDocument', 'POST', document);
+
+const updateDocument = async (document: {}) =>
+    await requestWithBody('/updateDocument', 'PUT', document);
+
+const deleteDocument = async (id: string) =>
+    await request(`/deleteDocument?id=${id}`, 'DELETE');
+
 const Api = {
   healthCheck,
   getDocuments,
+  addDocument,
+  updateDocument,
+  deleteDocument,
 };
 
 export default Api;
