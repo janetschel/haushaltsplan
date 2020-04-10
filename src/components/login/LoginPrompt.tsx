271a35ac00661390b270a7633bc8f923eab19228
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-04-10 11:50:37 +0200

    Added CircularProcess to show user he is logging
    Showing a CircularProcess now when the user is logging in and the backend is processing the request. If the backend currently sleeps in production or is under heavy workload the user previously would have no indication that anything happens when logging in
    Also I added a error message if the credentials don't match any user registered

--- src/components/login/LoginPrompt.tsx
+++ src/components/login/LoginPrompt.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import {Button, TextField, Typography} from '@material-ui/core';
+import {Button, TextField, Typography, CircularProgress } from '@material-ui/core';
 
-class LoginPrompt extends React.Component<Props, {}> {
 
-  userRequestingLogIn = () => {
+class LoginPrompt extends React.Component<Props, { loggingIn: boolean, falseCredentials: boolean }> {
+  constructor({props}: { props: any }) {
+    super(props);
+
+    this.state = {
+      loggingIn: false,
+      falseCredentials: false,
+    }
+  }
+
+  userRequestingLogIn = async () => {
     const { userLoggingIn } = this.props;
+
+    this.setState({ loggingIn: true });
+    this.setState({ falseCredentials: false });
 
     const username = (document.getElementById('username') as HTMLInputElement).value;
     const password = (document.getElementById('password') as HTMLInputElement).value;
 
-    userLoggingIn(username, password);
+    const userIsLoggedIn = await userLoggingIn(username, password);
+
+    if (!userIsLoggedIn) {
+      this.setState({ loggingIn: false });
+      this.setState({ falseCredentials: true });
+    }
   };
 
   keyPressed = (e: React.KeyboardEvent<HTMLDivElement>) => {
@@ -19,6 +36,8 @@ class LoginPrompt extends React.Component<Props, {}> {
   };
 
   render() {
+    const { loggingIn, falseCredentials } = this.state;
+
     return (
         <div className="LoginPrompt">
           <Typography variant="h4">Haushaltsplaner - Login</Typography>
@@ -33,13 +52,16 @@ class LoginPrompt extends React.Component<Props, {}> {
           />
           <br />
           <Button className="loginButton" variant="outlined" onClick={this.userRequestingLogIn}>Einloggen</Button>
+          <br />
+          { loggingIn && <CircularProgress className="waitingForLoginResponse"/> }
+          { falseCredentials && <Typography color="secondary">Benutzername und/oder Passwort falsch!</Typography> }
         </div>
     );
   }
 }
 
 type Props = {
-  userLoggingIn: (username: string, password: string) => void,
+  userLoggingIn: (username: string, password: string) => Promise<boolean>,
 }
 
 export default LoginPrompt;
